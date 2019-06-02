14892c790e9a4a7bf49eec706b0f0afc86bd1903
kfth <nieuwleven33@gail.com>
2019-06-02 12:06:53 +0200

    week1: api routes mongo models

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('*', (req, res) => res.send('DevConnector Home Page'));
+//Connect Database
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+//Define routes
+app.get('/', (req, res) => res.send('DevConnector Home Page'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () => console.log('server is running on port: ', PORT));
