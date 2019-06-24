b1634472b47fdaf98350696b3f963a99ee8546b3
kfth <nieuwleven33@gmail.com>
2019-06-25 00:47:48 +0200

    dev client side files with react and redux

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
+
 const PORT = process.env.PORT || 5000;
 
 //Connect Database
@@ -8,12 +10,19 @@ connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
-//Define routes
-app.get('/', (req, res) => res.send('DevConnector Home Page'));
 
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log('server is running on port: ', PORT));
